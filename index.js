905a21d3c3f83c86b6957d474892bc9c9b77ce76
shawn <blackskynine@gmail.com>
2021-12-14 16:05:32 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://shawn:1234@boilerplate.7y9ku.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!~~안녕하세요 ~')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+})
+
